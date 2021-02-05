35b446fdd7fffe03bed0955e3236f9aada7d9dc4
valdir-ti <valdir.ti@gmail.com>
2021-02-04 21:56:38 -0300

    Revert "Firs"
    This reverts commit 1cd9f9975b60b8817753320834ef65a872265996.

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express'
-
-const routes = Router();
-
-routes.get('/', (req: Request, resp: Response) => {
-    return resp.json({msg: 'API!'});
-});
-
-export default routes;
